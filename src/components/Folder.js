b6255ed7dbeac0fd05b0fc77e2d9bae185c4276c
MxJRd <reed182@mail.chapman.edu>
2020-04-14 11:01:14 -0700

    changes

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -5,12 +5,12 @@ import Notes from './Notes'
 
 class Folder extends React.Component {
 
-
 render(){
+    // console.log("this is the folder store",this.props.store) receives store from index.js
     return(
         <div>
             <h1> Folder  </h1>
-            {/* <Notes /> */}
+            <Notes store={this.props.store}/>
         </div>
     )
 }
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -3,11 +3,12 @@ import { Route } from 'react-router-dom'
 
 class Notes extends React.Component {
 
-
 render(){
+    // console.log(this.state.notes)
     return(
+        
         <div>
-
+        <button onClick={this.getNotes}>This is a button</button>
         <p> Notes</p>
 
         </div>
